54eac9daf9feaaf0148134e87dbba373a44d91c8
Mykola Kryvoruchko <krivoruchko.kolya@gmail.com>
2024-09-06 17:09:59 +0300

    add feature to "see more" button

--- src/js/main-container.js
+++ src/js/main-container.js
@@ -2,50 +2,6 @@ import { allCategories } from "./data";
 import { getApiData } from "./axios";
 const mainContainer = document.querySelector('.main-container');
 
-{/* <section>
-    <a href="">
-      <h4>CATEGORY ONE</h4>
-    </a>
-    <ul>
-      <li>
-        <a href="">
-          <img src="">
-          <p class="book-name">name of book</p>
-          <p class="author-name">author</p>
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <img src="">
-          <p class="book-name">name of book</p>
-          <p class="author-name">author</p>
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <img src="">
-          <p class="book-name">name of book</p>
-          <p class="author-name">author</p>
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <img src="">
-          <p class="book-name">name of book</p>
-          <p class="author-name">author</p>
-        </a>
-      </li>
-      <li>
-        <a href="">
-          <img src="">
-          <p class="book-name">name of book</p>
-          <p class="author-name">author</p>
-        </a>
-      </li>
-    </ul>
-    <button class="see-more-btn">SEE MORE</button>
-  </section> */}
-
 export async function generateMainContainer(){
   let generalHTML = `
     <h1>Best Sellers <span class="books-word">Books</span></h1>
@@ -72,17 +28,60 @@ export async function generateMainContainer(){
           </a>
         </li>
       `;
-      console.log(book);
-    })
-  }
-
-  generalHTML += `
+    });
+    generalHTML += `
       </ul>
-      <button class="see-more-btn">SEE MORE</button>
+      <button class="see-more-btn" data-category="${category.list_name}">SEE MORE</button>
     </section>
   `;
+  }
 
   mainContainer.innerHTML = generalHTML;
-  //fix bugs
-  //add event listener to buttons;
+  initializeSeeMoreBtn();
 }
+
+function initializeSeeMoreBtn(){
+  const seeMoreBtn = document.querySelectorAll('.see-more-btn');
+  seeMoreBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      loadCategory(e.target.dataset.category);
+    });
+  });
+}
+
+async function loadCategory(category){
+  let generalHTML = `
+    <h1>${category}</h1>
+  `;
+  const categoryBooks = await getApiData(`/books/category?category=${category}`);
+  let counter = 0;
+  categoryBooks.forEach((book) => {
+    if(counter % 5 == 0){
+      generalHTML += `
+      <section>
+        <ul>
+    `;
+    }
+
+    generalHTML += `
+      <li>
+        <a href="">
+          <img src="${book.book_image}">
+          <p class="book-name">${book.title}</p>
+          <p class="author-name">${book.author}</p>
+        </a>
+      </li>
+    `;
+
+    counter++
+    if(counter % 5 == 0){
+      generalHTML += `
+          </ul>
+        </section>
+      `;
+    }
+  });
+
+  mainContainer.innerHTML = generalHTML;
+}
+
