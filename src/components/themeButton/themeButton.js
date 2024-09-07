1c0330bb86e79c66258c9ba091d27e3a529f63f8
Mykhailo <matsyshynmykhailo@gmail.com>
2024-09-07 15:00:05 +0300

    fix support Ukraine container

--- src/components/themeButton/themeButton.js
+++ src/components/themeButton/themeButton.js
@@ -1,17 +1,13 @@
-// Отримуємо елемент перемикача теми
 const toggleSwitch = document.querySelector(
   '.theme-switch-wrapper input[type="checkbox"]'
 );
 
-// Перевірка системної теми користувача
 const systemPrefersDark = window.matchMedia(
   '(prefers-color-scheme: dark)'
 ).matches;
 
-// Перевірка збереженої теми в localStorage
 const currentTheme = localStorage.getItem('theme');
 
-// Якщо є збережена тема, використовуємо її, інакше беремо системну
 if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
   if (currentTheme === 'dark') {
@@ -19,21 +15,19 @@ if (currentTheme) {
   }
 } else if (systemPrefersDark) {
   document.documentElement.setAttribute('data-theme', 'dark');
-  toggleSwitch.checked = true; // Відзначаємо перемикач для темної теми
+  toggleSwitch.checked = true;
 } else {
   document.documentElement.setAttribute('data-theme', 'light');
 }
 
-// Функція для перемикання теми
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark'); // Зберігаємо вибір темної теми
+    localStorage.setItem('theme', 'dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light'); // Зберігаємо вибір світлої теми
+    localStorage.setItem('theme', 'light');
   }
 }
 
-// Додаємо слухач для перемикання теми
 toggleSwitch.addEventListener('change', switchTheme, false);
