42fbc470cb50454c8feecf1b4de7f6ea2fdf8321
Mykhailo <matsyshynmykhailo@gmail.com>
2024-09-08 09:52:50 +0300

    fix deploy

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 axios.defaults.baseURL = 'https://books-backend.p.goit.global';
 
-export async function getApiData(url){
-  try{
+export async function getApiData(url) {
+  try {
     const response = await axios.get(url);
     return response.data;
-  }catch(error){
+  } catch (error) {
     console.log(error);
     throw error;
   }
